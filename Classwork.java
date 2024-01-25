3e3fc68f732e59694e04d4b0f3ab1e741858959a
Loredana966 <157520431+Loredana966@users.noreply.github.com>
2024-01-25 19:14:18 +0200

    Add files via upload

---
+++ Classwork.java
@@ -0,0 +1,21 @@
+public class Classwork {
+    public static void main(String[] args) {
+        System.out.println("Hello if!");
+
+        int x = 20, y = 30;
+        if (x < y) {
+            System.out.println("x<y");
+        } else if (x==y) {
+            System.out.println("x=y");
+        } else {
+            System.out.println("x>y");
+        }
+
+        int a=21;
+        if (a%2==0){
+            System.out.println("Este o valoare para");
+        }else {
+            System.out.println("Este o valoare impara");
+        }
+    }
+}
